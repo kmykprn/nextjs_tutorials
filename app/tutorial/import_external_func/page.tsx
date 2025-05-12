6d7482074a0d91c7a769d5a1e6c824159db65860
kmykprn <kmykprn@gmail.com>
2025-05-12 13:25:29 +0900

    外部で定義した関数を呼び出すチュートリアルの実装

---
+++ app/tutorial/import_external_func/page.tsx
@@ -0,0 +1,9 @@
+import ImageSelectCard from "@/app/tutorial/import_external_func/SelectCard"
+
+export default function ImportExternalFunc() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <ImageSelectCard />
+    </div>
+  )
+}
