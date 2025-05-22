da8a7cd3207c5b03c8c382e17166139ed0f15a02
kmykprn <kmykprn@gmail.com>
2025-05-22 11:57:04 +0900

    コンポーネントの場所を固定する実装の追加

---
+++ app/tutorial/fixposition/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="fixed top-4 left-4 w-64 h-48">
+      <h1>About Page</h1>
+      <p>これは page_tutorial ページです。</p>
+    </div>
+  )
+}
